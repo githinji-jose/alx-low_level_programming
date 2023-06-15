5350dddb18c1bc5b503b81f1ba0e825610b847d6
joseph githinji <josegesho2001@gmail.com>
2023-06-15 08:50:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ /**
+  *  a programme that prints Programming is like building a multilingual puzzlethen followed by a new line
+  *  return 0
+  */
+in main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
