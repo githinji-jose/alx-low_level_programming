166955d6dbb16a7ff3846ca1e57b91195e719a7d
joseph githinji <josegesho2001@gmail.com>
2023-06-15 10:02:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
  /**
-  * main -  a programme that prints a program and enters a new line
+  * main -a programme that prints a program and enters a new line
   *  return 0
   */
 int main(void)
