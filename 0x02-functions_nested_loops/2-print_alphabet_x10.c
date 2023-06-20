9a44342d24ec08810f970406d71fc14e87dfa2d9
joseph githinji <josegesho2001@gmail.com>
2023-06-20 11:18:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ *   main - check the code.
+ *    Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		i++
+	}
+}
