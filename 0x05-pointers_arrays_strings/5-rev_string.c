e645ca21d7a1250b1989a1060bb39fc7a73b4133
joseph githinji <josegesho2001@gmail.com>
2023-06-26 12:23:46 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: pointer of string
+ * return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+
+		return (i);
+}
