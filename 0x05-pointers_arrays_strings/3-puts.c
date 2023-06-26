d2a7b255c51f6955a4f083d5545a4dfed5af21be
joseph githinji <josegesho2001@gmail.com>
2023-06-26 11:59:59 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string, followed byvnew line
+ * @str: pointer for string to print
+ * return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	while (s[i])
+	{
+		_putchar(strg[i]);
+		i++
+	}
+	_putchar('\n');
+}
