99817a185f33045f6a75862612847e0e60f6502b
joseph githinji <josegesho2001@gmail.com>
2023-06-27 11:32:20 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,12 +4,11 @@
 /**
  * print_array - prints n elements of an array
  * @a: int type array pointer
- * @n: int type integer
- * retyrn: void
+ * @n: intvtype integer
+ * return: void
  */
 void print_array(int *a, int n)
 {
-	int i;
 	int i = 0;
 
 	for (n--; n >= 0; n--, i++)
